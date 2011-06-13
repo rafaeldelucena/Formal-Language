3d28dba61745a416f8092632cb2b7eba4526f9f0
Rafael <rafael@mississipi.(none)>
2011-06-13 01:47:20 -0300

    version 0.1.2
    Committer: Rafael <rafaeldelucena@gmail.com>

--- estado.rb
+++ estado.rb
@@ -1,38 +1,50 @@
+require 'set'
 require 'transicao'
 
 class Estado
-	def initialize(tag, final)
+	def initialize(tag)
 		@tag = tag
-		@final = final
-		@transicoes = []
-		@destinos = []
+		@inicial = false
+		@final = false
+		@transicoes = Set.new
 	end
 	def addTransicao(destino, elemento)
-		@transicoes << Transicao.new(self, destino, elemento)
+		@transicoes.add(Transicao.new(self, destino, elemento))
 	end
-        def to_s
-                "#{@tag}"
-        end
-        def equal?(estado)
-                (@tag == estado.tag && @final == estado.final) ? true : false
-        end
-        
-        def getDestino(elemento)
-                getTransicao(elemento).collect {|transicao|
-                        transicao.destino
-                }
-        end
-        def getTransicao(elemento)
-                @transicoes.select {|transicao|
-                    transicao if elemento == transicao.elemento
-                }
-        end
-        def deterministico?
-                @transicoes.each {|transicao|
-                        return false if getDestino(transicao.elemento).size > 1
-                }
-			return true
-        end
-	 attr_writer :tag
-	 attr_reader :tag, :final, :destinos, :transicoes
+	def to_s
+		"#{@tag}"
+	end
+	def hash
+		@tag.hash
+	end
+	def eql?(estado)
+		self.hash == estado.hash
+	end
+	def inicial
+		@inicial = true
+	end
+	def inicial?
+		@inicial
+	end
+	def final
+		@final = true
+	end
+	def final?
+		@final
+	end
+	def getDestino(elemento)
+		getTransicao(elemento).collect {|transicao| transicao.destino }
+	end
+	def getTransicao(elemento)
+		@transicoes.select {|transicao| transicao if elemento.eql?(transicao.elemento)
+		}
+	end
+	def deterministico?
+		@transicoes.each {|transicao|
+			return false if getDestino(transicao.elemento).size > 1
+			}
+		return true
+	end
+	attr_writer :tag
+	attr_reader :tag, :transicoes
 end
