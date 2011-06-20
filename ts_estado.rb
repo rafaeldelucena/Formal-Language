edb5ea1818cc51ac2bc52704bd4c0c0c6f6cceb7
Daniel Neis Araujo <daniel@npd.ufsc.br>
2011-06-20 00:28:21 -0300

    version 0.1.4.1

--- ts_estado.rb
+++ ts_estado.rb
@@ -4,20 +4,18 @@ require 'estado'
 class TesteEstado < Test::Unit::TestCase
         def setup
                 @q0 = Estado.new("q0")
-				@q0.final
+		@q0.final
                 @q1 = Estado.new("q1")
                 @q2 = Estado.new("q0")
-				@q2.final
-                
-				@q1.addTransicao(@q1, "b")
-
-				@q0.addTransicao(@q1, "b")
+		@q2.final
+              	@q1.addTransicao(@q1, "b")
+		@q0.addTransicao(@q1, "b")
               	@q0.addTransicao(@q0, "a")
-				@q0.addTransicao(@q1, "a")
+		@q0.addTransicao(@q1, "a")
                 
                 @transicao = Transicao.new(@q0, @q1, "b")
                 @transicao2 = Transicao.new(@q0, @q1, "a")
-				@transicao3 = Transicao.new(@q0, @q1, "a")        
+		@transicao3 = Transicao.new(@q0, @q1, "a")        
         end 
         def test_estado
                 assert_equal("q0", @q0.tag)
@@ -40,7 +38,7 @@ class TesteEstado < Test::Unit::TestCase
         end
         def test_get_destinos
                 assert_equal(true, @q0.getDestino("a").include?(@transicao2.destino))
-				assert_equal(2, @q0.getDestino("a").size)
+		assert_equal(2, @q0.getDestino("a").size)
         end
 	def test_deterministico?
 		assert_equal(false, @q0.deterministico?)
